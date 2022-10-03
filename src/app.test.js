bc883e3ea4127f9fc1b3a859b78bef5545bf7bfc
AllanKonecny1 <104767955+AllanKonecny1@users.noreply.github.com>
2022-10-03 18:47:13 +0200

    changed the action + added next js app

---
+++ src/app.test.js
@@ -0,0 +1,15 @@
+const dayOfTheWeek = (date = new Date()) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return days[date.getDay()];
+}
+
+
+try {
+    document.getElementById('day').innerText = dayOfTheWeek();
+} catch (err) {
+    console.error(err)
+}
+
+
+exports.dayOfTheWeek = dayOfTheWeek;
